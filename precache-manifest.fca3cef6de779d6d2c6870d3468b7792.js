cd85fee915579b459d532e374b5a18aa164482d9
huangcrab <huangcrab@gmail.com>
2019-01-16 09:37:15 -0500

    Update

---
+++ precache-manifest.fca3cef6de779d6d2c6870d3468b7792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7dd00683960e59a59662",
+    "url": "/static/js/main.7dd00683.chunk.js"
+  },
+  {
+    "revision": "1836e251c73ac319ed82",
+    "url": "/static/js/1.1836e251.chunk.js"
+  },
+  {
+    "revision": "7dd00683960e59a59662",
+    "url": "/static/css/main.93329ba5.chunk.css"
+  },
+  {
+    "revision": "71c3bf27bd58b26a0a9bbd193cab811c",
+    "url": "/index.html"
+  }
+];
